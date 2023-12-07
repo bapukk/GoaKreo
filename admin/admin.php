d56b4896df9f8d321c37e01e9e69d4ecfca68514
bapukk <farihnailul1@gmail.com>
2023-12-07 10:27:02 +0700

    mengubah admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -1,64 +1,58 @@
 <?php
 include '../function.php';
-$event = query("SELECT * FROM event");
+$events = query("SELECT * FROM event");
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>admin</title>
-  <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="admin.css">
+    <title>dashboarad</title>
 </head>
 
 <body>
-  <header>
-    <nav class="navbar">
-      <ul>
-        <li><a href="#">+</a></li>
-        <li><a href="#">ADMIN</a></li>
-        <li><a href="#">Logout</a></li>
-      </ul>
-      
-    </nav>
-  </header>
-  <main>
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Gambar</th>
-          <th>Judul</th>
-          <th>Deskripsi</th>
-          <th>Tanggal</th>
-          <th>Aksi</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php $i = 1 ?>
-        <?php foreach ($event as $evn): ?>
-          <tr>
-            <td>
-              <?= $i ?>
-            </td>
-            <td><img src="../img/<?= $evn["gambar"]; ?>" alt="Gambar 1" width="60"></td>
-            <td>
-              <?= $evn["nama"]; ?>
-            </td>
-            <td>
-              <?= $evn["deskripsi"]; ?>
-            </td>
-            <td>
-              <?= $evn["tanggal"]; ?>
-            </td>
-            <td><button>Edit</button> <button>Delete</button></td>
-          </tr>
-          <?php $i++ ?>
-        <?php endforeach; ?>
-      </tbody>
-    </table>
-  </main>
+
+    <div class="container">
+        <div class="header">
+            <div class="title" id="title">
+                <h1>EVENT</h1>
+            </div>
+        </div>
+            <div class="logout">
+                <a href="logout.php"><button class="button">Logout</button></a>
+            </div>
+            <div class="tambah">
+                <a href="tambah.php"><button class="button2">Tambah</button></a>
+            </div>
+        <div class="tabel">
+            <table>
+                <tr>
+                    <th>NO</th>
+                    <th>GAMBAR</th>
+                    <th>NAMA</th>
+                    <th>DESKRIPSI</th>
+                    <th>TANGGAL</th>
+                    <th colspan="2"></th>
+                </tr>
+                <?php $i = 1; ?>
+                <?php foreach( $events as $event ) : ?>
+                <tr>
+                    <td><?= $i; ?></td>
+                    <td><img src="../img/<?= $event["gambar"]; ?>" alt="" id="gambar" width="60"></td>
+                    <td><?= $event["nama"]; ?></td>
+                    <td><?= $event ["deskripsi"]; ?></td>
+                    <td><?= $event["tanggal"]; ?></td>
+                    <td><a href="edit.php?id=<?= $event["id"]; ?> & <?= $event["gambar"]; ?> "><button class="button3">edit</button></a></td>
+                    <td><button class="button4">delete</button></td>
+                </tr>
+                <?php $i++; ?>
+                <?php endforeach; ?>  
+            </table>
+        </div>
+    </div>
+
 </body>
 
 </html>
